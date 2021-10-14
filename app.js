777be060cc8c32d5c05725091995870c3f57a2a7
Jonathan Mendonca <jonodmend@hotmail.com>
2021-10-14 21:56:51 +0100

    Add validation

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-var sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+// const sensor = require('node-dht-sensor');
 require('dotenv').config();
 
 // Express
 const app = express();
 const port = 3000;
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 // SQLLite
 const db = new sqlite3.Database(':memory:');
@@ -19,8 +22,30 @@ const authenticate = (req, res, next) => {
   }
 };
 
+const validate = (req, res, next) => {
+  if (req.body.hasOwnProperty('timestamp') &&
+    req.body.hasOwnProperty('indoorTemp') && req.body.hasOwnProperty('indoorHumidity') &&
+    req.body.hasOwnProperty('outdoorTemp') && req.body.hasOwnProperty('outdoorHumidity')) {
+    next();
+  } else {
+    return res.status(400).send({
+      message: 'Bad request'
+    });
+  }
+};
+
 app.get('/', authenticate, (req, res) => {
   res.send({ message: 'Hello' });
+});
+
+app.post('/weather', authenticate, validate, async (req, res) => {
+  res.send({ message: "Hello" });
+});
+
+app.get('*', function(req, res) {
+  return res.status(400).send({
+    message: 'Bad request'
+  });
 });
 
 app.listen(port, () => {
